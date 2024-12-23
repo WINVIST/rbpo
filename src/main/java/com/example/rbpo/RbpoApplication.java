9d97bc62b5ff25a255806b29ef260848de4e145a
winvist <winvist23@gmail.com>
2024-12-23 18:02:55 +0300

    new files

--- src/main/java/com/example/rbpo/RbpoApplication.java
+++ src/main/java/com/example/rbpo/RbpoApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RbpoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RbpoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RbpoApplication.class, args);
+    }
 
 }
--- src/test/java/com/example/rbpo/RbpoApplicationTests.java
+++ src/test/java/com/example/rbpo/RbpoApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class RbpoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
