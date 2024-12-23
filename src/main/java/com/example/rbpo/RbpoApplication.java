8799d94f0500b6f5850ce5995c1a5ad47aecfca0
winvist <winvist23@gmail.com>
2024-12-23 18:02:32 +0300

    Initial commit

---
+++ src/main/java/com/example/rbpo/RbpoApplication.java
@@ -0,0 +1,13 @@
+package com.example.rbpo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RbpoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RbpoApplication.class, args);
+	}
+
+}
